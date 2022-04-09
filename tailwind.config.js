1da71d4d1ab7b7e7037f7e8c11c0a50689e92363
JaydenDev <willsjayden2@gmail.com>
2022-04-09 09:31:07 -0500

    init

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: ["./index.html"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+    animation: {
+        'spin-slow': 'spin 3s linear infinite',
+    }
+}
